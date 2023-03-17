32c7f1b38f77d15c21ac7522a08168432521f048
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-03-16 19:50:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * followed by a line
- * Return: 1
+ * main - A C program that prints a standard error
+ * Return: 1 (Success)
 */ int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
