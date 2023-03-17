83c440c3e85d7f2483697adbb0d23246daa5a85b
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-03-16 20:01:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a standard error
- * Return: 1 (Success)
+ * main - Entry
+ * Return: 1 
 */ int main(void)
 {
 fput("1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
