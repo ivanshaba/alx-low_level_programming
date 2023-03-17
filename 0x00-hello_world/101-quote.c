4276b58b52b6613127b4003a57271e5f9f54cca1
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-03-16 19:59:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  * Return: 1 (Success)
 */ int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
