09329626b806f204b4438dd3e499b66b806c4bb2
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-03-17 00:20:32 -0700

    #include <stdio.h>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  * Return: 1
 */ int main(void)
 {
-fput("1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	fput("1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+       	return (1);
 }
