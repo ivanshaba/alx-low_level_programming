bcb4d87b24cbf11e50f2d0e802734d0a24336bdc
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-03 14:40:37 -0700

    ptr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,10 +4,11 @@
  *  It then uses a loop to fill each byte in the memory area with the constant byte b 
  *  Finally, it returns the pointer to the memory area s.
 */  
-char *_memset(char *s, char b, unsigned int n) {
-    unsigned int i;
+char *_memset(char *s, char b, int n)
+{
+    int i;
     for (i = 0; i < n; i++) {
         s[i] = b;
     }
-    return s;
+    return (s);
 }
