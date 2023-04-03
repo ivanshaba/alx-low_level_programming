8ed8e8f21f3d0049e77206ed3a208a4de8a9587b
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-03 15:15:13 -0700

    what is wrong with this code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,9 @@
 /**
  * function takes in a pointer to a memory area s, a constant byte b, and the number of bytes n to fill with the constant byte
  *  It then uses a loop to fill each byte in the memory area with the constant byte b 
- *  Finally, it returns the pointer to the memory area s.
-*/ 
+ *  Finally, it returns the pointer to the memory area s
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
