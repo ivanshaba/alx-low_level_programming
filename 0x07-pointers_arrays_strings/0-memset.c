3a36ab87142f74ded0649a36702ad957c2704352
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-03 15:38:20 -0700

    ptr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 /**
- * function takes in a pointer to a memory area s, a constant byte b, and the number of bytes n to fill with the constant byte
- *  It then uses a loop to fill each byte in the memory area with the constant byte b 
- *  Finally, it returns the pointer to the memory area s
-*/
+ * _memset -  function sending a constant to memory
+ * declare an unsigned int
+ * because we are storing avalue that will always be
+ * non-negative (zero or positive)
+ * Fill each byte in the block of memory
+ * pointed to by s with the value of b
+*/ 
+char *_memset(char *s, char b, unsigned int){
+	unsigned int i;
 
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i = 0;
-	while (i < n){
-	s[i] = b;
-	i++;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
 	return (s);
-}
+}i
