ac2cdd34e4db82df4ceb90c6c3e10a6a46abed9c
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-05 15:50:31 -0700

    dnf

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,15 +7,15 @@
  * Return: the factorial of the given number, or -1 if the number is negative.
  */
 
-int factorial(int num)
+int factorial(int n)
 {
 	if (num < 0) // check if number is negative
 	{
 		return (-1); // return -1 if the number is negative
 	}
-	if (num == 0) // check if number is 0
+	if (n == 0) // check if number is 0
 	{
 		return (1); // return 1 if the number is 0
 	}
-	return (num * factorial(num - 1)); // calculate factorial using recursion
+	return (n * factorial(num - 1)); // calculate factorial using recursion
 }
