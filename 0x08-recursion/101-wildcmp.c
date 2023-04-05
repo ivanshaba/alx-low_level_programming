66201d103a86d5a8ceba2d43244ccb6c169da6c9
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-05 16:29:47 -0700

    jdbjf

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-wildcmp - Compare two strings for a match
-@s1: pointer to first string
-@s2: pointer to second string
-Return: 0 on failure, 1 on success
+ * wildcmp - Compare two strings for a match
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * Return: 0 on failure, 1 on success
 */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0')
-{
-if (*s2 != '\0' && s2 == '')
-{
-return (wildcmp(s1, s2 + 1));
-}
-return (*s2 == '\0');
-}
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && s2 == '')
+		{
+
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
