4f4f58979ea2b9097b7f6f7662151629ee5ea3c4
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-05 14:54:51 -0700

    puts3🥺

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  * my_reverse_print - prints a string in reverse recursively.
  * @str: pointer to string parameter.
- * Return: nothing.
+ * Return: nothinig.
 */
-void _print_rev_recursion(char *str)
+void _print_rev_recursion(char *s)
 {
-    if (*str)
-    {
-        _print_rev_recursion(str + 1);
-        _putchar(*str);
-    }
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
