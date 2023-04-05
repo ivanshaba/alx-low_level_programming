a349c85af4bfec17da5b3ebba0d596ab304e10a9
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-05 13:53:26 -0700

    puts_recursion😂😬👨‍💻☝️

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
-my_puts_recursion - recursively print a string.
-@str: pointer to a character string.
-Return: none.
+ * *my_puts_recursion - recursively print a string.
+ * *@str: pointer to a character string.
+ * *Return: none.
 */
 void _puts_recursion(char *str)
 {
-// Check if the current character is not the null terminator
 if (*str != '\0')
 {
-// Print the current character and recursively call the function with the next character
 _putchar(*str);
 _puts_recursion(str + 1);
 }
 else
-{
-// If we've reached the end of the string, print a newline character
 _putchar('\n');
 }
 }
