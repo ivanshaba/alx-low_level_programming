2b2f4d3c4ac5a44f18e876000305c8229970c420
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-05 15:03:02 -0700

    jd

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * my_reverse_print - prints a string in reverse recursively.
  * @str: pointer to string parameter.
+ *
  * Return: nothinig.
-*/
-void _print_rev_recursion(char *s)
+ */
+void my_reverse_print(char *str)
 {
-	if (*s == '\0')
-	{
-		return;
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
-	}
+    if (*str)
+    {
+        my_reverse_print(str + 1);
+        _putchar(*str);
+    }
 }
