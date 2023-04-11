7f0f0baef54441d8debfb6c0e9afecd92cd89c19
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-11 15:29:55 -0700

    jdnfdkj

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
-str_concat - concatenates two strings
-@s1: first string
-@s2: second string
-Return: pointer to a newly allocated space in memory which
-contains the contents of s1, followed by the contents of s2,
-and null terminated, NULL on failure
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
 */
 char *str_concat(char *s1, char *s2)
 {
-int i, j, len1 = 0, len2 = 0, len;
-char *result;
-scss
-Copy code
- if (s1 != NULL)
- {
-         i = 0;
-         while (s1[i] != '\0')
-         {
-                 len1++;
-                 i++;
-         }
- }
+        int i, j, len1, len2, len;
+        char *result;
 
- if (s2 != NULL)
- {
-         i = 0;
-         while (s2[i] != '\0')
-         {
-                 len2++;
-                 i++;
-         }
- }
+        len1 = len2 = 0;
 
- len = len1 + len2;
- result = malloc(sizeof(char) * (len + 1));
- if (result == NULL)
-         return (NULL);
+        if (s1 != NULL)
+        {
+                i = 0;
+                while (s1[i++] != '\0')
+                        len1++;
+        }
 
- for (i = 0; i < len1; i++)
-         result[i] = s1[i];
+        if (s2 != NULL)
+        {
+                i = 0;
+                while (s2[i++] != '\0')
+                        len2++;
+        }
 
- for (j = 0; j < len2; j++, i++)
-         result[i] = s2[j];
+        len = len1 + len2;
+        result = (char *)malloc(sizeof(char) * (len + 1));
+        if (result == NULL)
+                return (NULL);
 
- result[len] = '\0';
+        for (i = 0; i < len1; i++)
+                result[i] = s1[i];
+        for (j = 0; j < len2; j++, i++)
+                result[i] = s2[j];
+        result[len] = '\0';
 
- return (result);
+        return (result);
 }
