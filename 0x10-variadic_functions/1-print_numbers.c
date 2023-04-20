9afc1c69dfa32273ba0e87b2f3fa5edde7fe42a7
ivan shaba <ndayishabaivanbreezy@gmail.com>
2023-04-20 16:03:24 -0700

    all

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,36 +1,31 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Prints a list of integers separated by a given separator.
- * @separator: The string separator to print between integers.
- * @n: The number of integers to print.
- * @...: The list of integers to print.
- *
- * Description: This function prints a given number of integers separated by a
- * specified separator. The integers are passed as variable arguments to the
- * function, using the C va_list mechanism. If the separator is NULL, an empty
- * string is used. The function prints a newline character at the end of the
- * list.
+ * print_numbers - prints numbers followed by a new line
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: Always 0
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list ap;
+	unsigned int i;
+	va_list ap;
 
-    va_start(ap, n);
+	va_start(ap, n);
 
-    /* If separator is NULL, use an empty string instead. */
-    if (separator == NULL)
-        separator = "";
+	if (separator == NULL)
+		separator = "";
 
-    /* Loop over each integer and print it, separated by the separator. */
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(ap, int));
-        if (i < n - 1)
-            printf("%s", separator);
-    }
-
-    printf("\n");
-    va_end(ap);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
+
+
+ 
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,40 +1,31 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - Prints a list of strings separated by a given separator.
- * @separator: The string separator to print between strings.
- * @n: The number of strings to print.
- * @...: The list of strings to print.
- *
- * Description: This function prints a given number of strings separated by a
- * specified separator. The strings are passed as variable arguments to the
- * function, using the C va_list mechanism. If a string in the list is NULL, the
- * string "(nil)" is printed instead. If the separator is NULL, an empty string
- * is used. The function prints a newline character at the end of the list.
+ * print_strings - Prints strings passed to the function separed by separators
+ * @separator: A string that is used to separate strings
+ * @n: number of unnamed parameters
+ * Return: Always 0
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    char *str;
-    unsigned int i;
-    va_list ap;
+	char *str;
+	unsigned int i;
+	va_list ap;
 
-    va_start(ap, n);
+	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
 
-    /* If separator is NULL, use an empty string instead. */
-    if (separator == NULL)
-        separator = "";
-
-    /* Loop over each string and print it, separated by the separator. */
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(ap, char*);
-        if (str == NULL)
-            str = "(nil)";
-        printf("%s", str);
-        if (i < n - 1)
-            printf("%s", separator);
-    }
-
-    printf("\n");
-    va_end(ap);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
